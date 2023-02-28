b781b4d5c7227a295b28dda9bee91f9ecd37e7ac
Daniel Barba <frdb103@gmail.com>
2023-02-27 23:06:01 -0500

    working api point and mysql

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2"
-import { service_acount } from "../service_account.js"
+import { service_account } from "../service_account.js"
 
 export function getTransactions(req, res) {
     //console.log("INSIDE FUNCTIONS")
-    const db = mysql.createConnection(service_acount)
+    const db = mysql.createConnection(service_account)
     db.query("SELECT * FROM transactions", (error, results) => {
         if (error) {
            // console.log(error)
