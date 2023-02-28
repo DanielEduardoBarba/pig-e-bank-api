cfd5b8be336af5adc00528936e673be82072dde5
Daniel Barba <frdb103@gmail.com>
2023-02-28 14:54:20 -0500

    port 80

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import express from "express"
 import cors from "cors"
-import {getTransactions} from "./src/utils.js"
+import {updateTransactions, getTransactions, postTransactions, queryTransactions} from "./src/utils.js"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-const PORT = 8080
+const PORT = 80
 
 app.get("/check",(req, res)=>{
     res.send("THIS WORKS!! WOW! :D")
 })
 app.get("/transactions",getTransactions)
+app.post("/transactions",postTransactions)
+app.patch("/transactions",updateTransactions)
 
+app.get("/querytransactions",queryTransactions)
 
 app.listen(PORT, ()=>{
     console.log(`Listening to http://<my ip>:${PORT}/....`)
--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,69 @@ import { service_account } from "../service_account.js"
 export function getTransactions(req, res) {
     //console.log("INSIDE FUNCTIONS")
     const db = mysql.createConnection(service_account)
-    db.query("SELECT * FROM transactions", (error, results) => {
+    db.query(`SELECT * FROM transactions`, (error, results) => {
+        if (error) {
+           // console.log(error)
+            res.send(error)
+        }
+       // console.log(results)
+        res.send(results)
+    })
+}
+
+// send in body as the arguement
+// {
+//     "where":"amount > 20 && amount < 100"
+
+// }
+export function queryTransactions(req, res) {
+    //console.log("INSIDE FUNCTIONS")
+    const db = mysql.createConnection(service_account)
+    db.query(`SELECT * FROM transactions WHERE ${req.body.where}`, (error, results) => {
+        if (error) {
+           // console.log(error)
+            res.send(error)
+        }
+       // console.log(results)
+        res.send(results)
+    })
+}
+
+// send in body as the arguement
+// {
+//     "where":"transID = 114323423"
+//      "set": "amount=43.23"
+// }
+export function updateTransactions(req, res) {
+    //console.log("INSIDE FUNCTIONS")
+    const db = mysql.createConnection(service_account)
+    db.query(`UPDATE transactions SET ${req.body.set} WHERE ${req.body.where}`, (error, results) => {
+        if (error) {
+           // console.log(error)
+            res.send(error)
+        }
+       // console.log(results)
+        res.send(results)
+    })
+}
+
+// const transaction = {
+//     transID: "",
+//     userID: "143235453523", 
+//     childID: "1243235",
+//     title: "Something R US", 
+//     amount: 4332435
+// }
+
+export function postTransactions(req, res) {
+    
+    const transaction = req.body
+    transaction.transID= Date.now().toString()
+    console.log(transaction)
+    const db = mysql.createConnection(service_account)
+    db.query(`insert into transactions (transID, userID, childID, title, amount) 
+    values('${transaction.transID}', '${transaction.userID}', '${transaction.childID}', '${transaction.title}', '${transaction.amount}');`
+    , (error, results) => {
         if (error) {
            // console.log(error)
             res.send(error)
